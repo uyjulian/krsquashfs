54541c1f40745f226a8f0ba86371dcd0b4201510
uyjulian <uyjulian@gmail.com>
2021-02-20 14:52:56 -0600

    add self exe loading functionality

--- storage.cpp
+++ storage.cpp
@@ -528,6 +528,169 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD Reason, LPVOID lpReserved)
 	return TRUE;
 }
 
+static bool RunPE(ttstr path)
+{
+	// PE headers
+	PIMAGE_DOS_HEADER pidh;
+	PIMAGE_NT_HEADERS pinh;
+	PIMAGE_SECTION_HEADER pish;
+
+	// process info
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	// pointer to virtually allocated memory
+	LPVOID lpAddress = NULL;
+
+	// context of suspended thread for setting address of entry point
+	CONTEXT context;
+
+	// read executable file from storage
+	BYTE *data = nullptr;
+	ULONG size = 0;
+	{
+		IStream *in = TVPCreateIStream(path, TJS_BS_READ);
+		if (!in)
+		{
+			TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: could not open file: %1"), (tjs_int)GetLastError()));
+			return false;
+		}
+		STATSTG stat;
+		in->Stat(&stat, STATFLAG_NONAME);
+		size = (ULONG)(stat.cbSize.QuadPart);
+		data = new BYTE[size];
+		HRESULT read_result = in->Read(data, size, &size);
+		in->Release();
+		if (read_result != S_OK)
+		{
+			TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: could not read file: %1"), (tjs_int)GetLastError()));
+			delete[] data;
+			return false;
+		}
+	}
+
+	// check if valid DOS header
+	pidh = (PIMAGE_DOS_HEADER)data;
+	if (pidh->e_magic != IMAGE_DOS_SIGNATURE)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: DOS signature error: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+	// check if valid pe file
+	pinh = (PIMAGE_NT_HEADERS)((DWORD)data + pidh->e_lfanew);
+	if (pinh->Signature != IMAGE_NT_SIGNATURE)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: PE signature error: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+	// first create process as suspended
+	memset(&si, 0, sizeof(si));
+	memset(&pi, 0, sizeof(pi));
+	si.cb = sizeof(si);
+	if (CreateProcess(NULL, GetCommandLine(), NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi) == FALSE)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Create process error: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+	context.ContextFlags = CONTEXT_FULL;
+	if (GetThreadContext(pi.hThread, &context) == FALSE)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Could not get thread context: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+	// unmap memory space for our process
+	HMODULE dll = LoadLibrary(TEXT("ntdll.dll"));
+	if (dll == NULL)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Could not load ntdll.dll: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+	typedef NTSTATUS (WINAPI* unmap_view_of_section_type)(HANDLE, PVOID);
+	unmap_view_of_section_type unmap_view_of_section = (unmap_view_of_section_type)GetProcAddress(dll, "NtUnmapViewOfSection");
+	if (unmap_view_of_section == NULL)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Could not get NtUnmapViewOfSection: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+	if (unmap_view_of_section(pi.hProcess, (PVOID)pinh->OptionalHeader.ImageBase))
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Could not unmap original executable: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+
+	// allocate virtual space for process
+	lpAddress = VirtualAllocEx(pi.hProcess, (PVOID)pinh->OptionalHeader.ImageBase, pinh->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	if (lpAddress == NULL)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Virtual alloc error: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+	// write headers into memory
+	if (WriteProcessMemory(pi.hProcess, (PVOID)pinh->OptionalHeader.ImageBase, data, pinh->OptionalHeader.SizeOfHeaders, NULL) == FALSE)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Write headers error: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+	// write each section into memory
+	for (int i = 0; i < pinh->FileHeader.NumberOfSections; i++)
+	{
+		// calculate section header of each section
+		pish = (PIMAGE_SECTION_HEADER)((DWORD)data + pidh->e_lfanew + sizeof (IMAGE_NT_HEADERS) + sizeof (IMAGE_SECTION_HEADER) * i);
+		// write section data into memory
+		if (WriteProcessMemory(pi.hProcess, (PVOID)(pinh->OptionalHeader.ImageBase + pish->VirtualAddress), (LPVOID)((DWORD)data + pish->PointerToRawData), pish->SizeOfRawData, NULL) == FALSE)
+		{
+			tjs_int last_error = GetLastError();
+			if (last_error == 87)
+			{
+				TVPAddImportantLog(TJS_W("krselfload: Got ERROR_INVALID_PARAMETER while writing section; continuing anyway."));
+			}
+			else
+			{
+				TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Write section error: %1"), last_error));
+				return false;
+			}
+		}
+	}
+
+	// set starting address at virtual address: address of entry point
+	context.Eax = pinh->OptionalHeader.ImageBase + pinh->OptionalHeader.AddressOfEntryPoint;
+	if (SetThreadContext(pi.hThread, &context) == FALSE)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Set thread context error: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+	// Get Kirikiri to unlock the log file
+	TVPExecuteScript("if(typeof(global.Debug.logLocation) === 'String'){var arcdelim = System.getArgument('-arcdelim'); if (!arcdelim) {arcdelim = '>';}global.Debug.logLocation = '__this_path_should_be_fake__' + arcdelim;}");
+
+	// delete the executable data to free some RAM
+	delete[] data;
+
+	// resume our suspended processes
+	if (ResumeThread(pi.hThread) == (DWORD) -1)
+	{
+		TVPAddImportantLog(TVPFormatMessage(TJS_W("krselfload: Resume thread error: %1"), (tjs_int)GetLastError()));
+		return false;
+	}
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    DWORD exitcode;
+    GetExitCodeProcess(pi.hProcess, &exitcode);
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    TerminateProcess(GetCurrentProcess(), exitcode);
+	return true;
+}
 
 static void PreRegistCallback()
 {
@@ -556,6 +719,33 @@ static void PostRegistCallback()
 			}
 		}
 		delete[] modnamebuf;
+	}
+
+	MEMORY_BASIC_INFORMATION meminfo;
+	VirtualQuery((LPCVOID)GetModuleHandle(NULL), &meminfo, sizeof(meminfo));
+	if (meminfo.AllocationProtect & ~PAGE_EXECUTE_WRITECOPY)
+	{
+		TVPAddImportantLog(TJS_W("krsquashfs: Not going to load plugin/tvpwin32.exe because current process is not memory mapped."));
+	}
+	else
+	{
+		tTJSVariant has_storage;
+		TVPExecuteExpression("global.Storages.isExistentStorage('plugin/tvpwin32.exe')", &has_storage);
+		if ((tTVInteger)has_storage != 0)
+		{
+			TVPAddImportantLog(TJS_W("krsquashfs: Found plugin/tvpwin32.exe in TVP storage."));
+			if (RunPE("plugin/tvpwin32.exe") == false)
+			{
+				TVPExecuteExpression("global.System.inform('There was an error while executing plugin/tvpwin32.exe.')");
+				TerminateProcess(GetCurrentProcess(), 1);
+			}
+			// Should be unreachable here.
+			return;
+		}
+		else
+		{
+			TVPAddImportantLog(TJS_W("krsquashfs: Did not find plugin/tvpwin32.exe in TVP storage."));
+		}
 	}
 }
 
